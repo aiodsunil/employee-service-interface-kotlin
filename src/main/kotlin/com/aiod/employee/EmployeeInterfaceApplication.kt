fd8e2e0917f42599adc7ff342ec3eb25b5b2dff1
sunil <aidosunil@gmail.com>
2018-07-28 12:38:37 +0100

    first commit

---
+++ src/main/kotlin/com/aiod/employee/EmployeeInterfaceApplication.kt
@@ -0,0 +1,11 @@
+package com.aiod.employee.employeeinterface
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmployeeInterfaceApplication
+
+fun main(args: Array<String>) {
+    runApplication<EmployeeInterfaceApplication>(*args)
+}
